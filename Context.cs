41e81e9bc636190f28ac8b625cc4268387a26420
Vinicius Climaco <climaco.vinicius@gmail.com>
2023-09-01 09:07:15 -0300

    Add project files.

---
+++ Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    public class Context
+    {
+        IChoice choice;        
+        public void SetChoice(IChoice choice)
+        {
+            this.choice = choice;
+        }        
+        public void ShowChoice()
+        {
+            choice.MyChoice();
+        }
+    }
+}
